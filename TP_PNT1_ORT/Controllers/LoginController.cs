2f2b43bec95e62a62c121c79af6268ffdab377a5
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2022-04-15 12:17:44 -0300

    Agregar archivos de proyecto.

---
+++ TP_PNT1_ORT/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TP_PNT1_ORT.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(
+            string email,
+            string password
+        ){
+
+            Console.WriteLine(email);
+            Console.WriteLine(password);
+
+            return null;
+
+
+        }
+    }
+}
